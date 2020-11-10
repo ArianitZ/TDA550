26ae9059e4b844ae79478c63a3e0ae20e42c8b84
ArianitZ <azeqiri95@gmail.com>
2020-11-10 13:26:38 +0100

    added cars superclass

--- Del_A/src/Cars.java
+++ Del_A/src/Cars.java
@@ -1,26 +1,17 @@
 import java.awt.*;
 
-public class Saab95{
+public abstract class Cars {
 
-    public boolean turboOn;
     public int nrDoors; // Number of doors on the car
     public double enginePower; // Engine power of the car
     public double currentSpeed; // The current speed of the car
     public Color color; // Color of the car
     public String modelName; // The car model name
-    
-    public Saab95(){
-        nrDoors = 2;
-        color = Color.red;
-        enginePower = 125;
-	    turboOn = false;
-        modelName = "Saab95";
-        stopEngine();
-    }
-    
+
     public int getNrDoors(){
         return nrDoors;
     }
+
     public double getEnginePower(){
         return enginePower;
     }
@@ -33,31 +24,19 @@ public class Saab95{
         return color;
     }
 
-    public void setColor(Color clr){
-	    color = clr;
+    private void setColor(Color clr){
+        color = clr;
     }
 
-    public void startEngine(){
-	    currentSpeed = 0.1;
+    private void startEngine(){
+        currentSpeed = 0.1;
     }
 
-    public void stopEngine(){
-	    currentSpeed = 0;
+    private void stopEngine(){
+        currentSpeed = 0;
     }
 
-    public void setTurboOn(){
-	    turboOn = true;
-    }
-
-    public void setTurboOff(){
-	    turboOn = false;
-    }
-    
-    public double speedFactor(){
-        double turbo = 1;
-        if(turboOn) turbo = 1.3;
-        return enginePower * 0.01 * turbo;
-    }
+    public abstract double speedFactor();
 
     public void incrementSpeed(double amount){
         currentSpeed = getCurrentSpeed() + speedFactor() * amount;
@@ -66,7 +45,7 @@ public class Saab95{
     public void decrementSpeed(double amount){
         currentSpeed = getCurrentSpeed() - speedFactor() * amount;
     }
-    
+
     // TODO fix this method according to lab pm
     public void gas(double amount){
         incrementSpeed(amount);
