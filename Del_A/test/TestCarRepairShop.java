4ab64d44bf6c201fc244268263b1cfc522e14a3d
verseg <verseg@student.chalmers.se>
2020-11-19 10:22:00 +0100

    test class

---
+++ Del_A/test/TestCarRepairShop.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+public class TestCarRepairShop {
+
+    @Before
+    public void init(){
+
+    }
+
+}
